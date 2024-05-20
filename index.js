eb35106d1dfbb5c27f23d07b2d6a52cdee36e808
steph-d989 <steb_d@hotmail.com>
2024-05-20 22:47:31 +0200

    falta 1,2,4,9

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 //RESUELVE LOS EJERCICIOS AQUÍ
 /* 1 */
-
+//const ana = empleados.find(empleado=>empleado.name==="Ana")
 /* 2 */
-
+//const luis = empleados.find(empleado=>empleado.name==="Luis");
+//const emailLuis = luis?luis.email:null;
+//console.log(emailLuis);
 /* 3 */
 let a = 5;
 let b = 3;
 [a, b] = [b, a];
+console.log(a)
+console.log(b)
+
 /* 4 */
-const {today: maximaHoy, tomorrow: maximaManana} = HIGH_TEMPERATURES;
+/* const {today: maximaHoy, tomorrow: maximaManana} = HIGH_TEMPERATURES;
 console.log(maximaHoy);
 console.log(maximaManana);
+ */
 /* 5 */
 function sumEveryOther(...num) {
     let suma = 0;
@@ -20,6 +26,7 @@ function sumEveryOther(...num) {
     return suma;
 }
 console.log(sumEveryOther(6, 8, 2, 3, 1));
+
 /* 6 */
 function addOnlyNums (...listado){
     let suma = 0;
@@ -31,6 +38,7 @@ function addOnlyNums (...listado){
     return suma;
 }
 console.log(addOnlyNums(1, "perro", 2, 4))
+
 /* 7 */
 function countTheArgs(...listado){
     return listado.length
@@ -42,26 +50,22 @@ function combineTwoArrays (elemento1,elemento2){
     return combinados;
 }
 console.log(combineTwoArrays([1, 2, 3], [4, 5, 6]));
+
 /* 9 */
 /* function onlyUniques(...elementos){
-    let unicos = [];
-    for(let i=0; i<elementos.length; i++){
-        unicos.push(elementos[i])
-    }
-    for(let j=0; i<unicos.length; j++){
-        if(elementos[i]===unicos[j]){
-            unicos.splice(unicos[j]);
-        }
-    }
-    return unicos;
+    let elementosUnicos = Set(elementos);
+    let array = Array.from(elementosUnicos);
+    return array;
 }
-onlyUniques("gato", "pollo", "cerdo", "cerdo"); */ 
-/* 10 */
-/* function combineAllArrays(arrays){
-    let arrayCombinado = [...arrays]
-    return arrayCombinado
+onlyUniques("gato", "pollo", "cerdo", "cerdo");
+
+*//* 10 */
+function combineAllArrays(...arrays){
+    let combinados = arrays.reduce((acumulador,actual)=>acumulador.concat(actual),[])
+    return combinados
 }
-combineAllArrays([2, 7, 3, 1], [2, 7, 4, 12], [2, 44, 22, 7, 3, 1]); */
+combineAllArrays([2, 7, 3, 1], [2, 7, 4, 12], [2, 44, 22, 7, 3, 1]);
+
 /* 11 */
 function sumAndSquare(...numeritos) {
     let argumento = [];
